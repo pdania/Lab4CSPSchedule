995e128c5d63bdfe7b067deb0f87dcad27fa22f9
Danylo Parkhomenko <pdania@ukr.net>
2020-10-26 16:54:21 +0200

    CSPAlgorithm added

---
+++ Lab3GeneticAlghoritm/CSPAlghoritm.cpp
@@ -0,0 +1,7 @@
+#include"CSPAlghoritm.h"
+
+CSPAlghoritm::CSPAlghoritm(Config& config,  size_t pairCount) :
+	_config(config),
+	_pairCount(pairCount),
+	_removedPairs(0) {
+}
---
+++ Lab3GeneticAlghoritm/CSPAlghoritm.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Config.h"
+#include "Course.h"
+#include "Room.h"
+#include "StudentsGroup.h"
+#include "Professor.h"
+#include <vector>
+#include <string>
+#include <map>
+#include <iomanip>
+#include<iostream>
+
+class CSPAlghoritm {
+public:
+
+public:
+
+	CSPAlghoritm(Config& config, size_t pairCount);
+
+	void Init();
+
+	bool CheckPairs();
+
+	void AddPairs();
+
+	void ProcessAlgorithm();
+
+	void PrintSchedule();
+
+private:
+	Config& _config;
+	std::vector<std::string> _population;
+
+	map<int, std::string> pairTimes = {
+		{1,"8:00-9:50"},
+		{2,"10:00-11:20"},
+		{3,"11:40-13:00"},
+		{4,"13:30-14:50"},
+		{5,"15:00-16:20"},
+		{6,"16:30-17:50"},
+		{7,"18:00-19:20"}
+	};
+
+	map<int, std::string> weekDays = {
+		{1,"Monday"},
+		{2,"Tuesday"},
+		{3,"Wednesday"},
+		{4,"Thursday"},
+		{5,"Friday"}
+	};
+
+	size_t _pairCount;
+	size_t _removedPairs;
+
+	string GeneratePair();
+
+};
